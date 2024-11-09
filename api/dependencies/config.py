93789b8b47632a65b6bd549ac22abcc7b1d18a7c
lan <133815596+lancanon@users.noreply.github.com>
2024-11-08 19:21:10 -0500

    update config

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -1,8 +1,8 @@
 class conf:
     db_host = "localhost"
-    db_name = "sandwich_maker_api"
+    db_name = "TaskManager"
     db_port = 3306
-    db_user = "root"
-    db_password = "rootroot"
+    db_user = "rollcall"
+    db_password = "Rollcall1234!"
     app_host = "localhost"
     app_port = 8000
